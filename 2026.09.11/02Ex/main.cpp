87fc8a7b3ae1e1ce281b64e566098f5ea91e1b26
AZEN-SGG <rascatov.alex@gmail.com>
2026-09-14 22:18:05 +0300

    Task 2 is done!

---
+++ 2026.09.11/02Ex/main.cpp
@@ -0,0 +1,31 @@
+#include "solve.h"
+
+#include <cstdio>
+#include <ctime>
+
+int
+main (int argc, char *argv[])
+{
+  int res;
+  double t;
+
+  if (!(argc == 3))
+    {
+      printf ("%s : filename k\n", argv[0]);
+      return -2;
+    }
+
+  t = clock ();
+  res = t2_solve (argv[1], argv[2]);
+  t = (clock () - t) / CLOCKS_PER_SEC;
+
+  if (res < 0)
+    {
+      fprintf(stderr, "%s : %d\n", argv[0], res);
+      return -1;
+    }
+
+  fprintf (stdout, "%s : %d\n", argv[0], res);
+
+  return 0;
+}
---
+++ 2026.09.11/02Ex/solve.h
@@ -0,0 +1,11 @@
+#ifndef SOLVE_H
+#define SOLVE_H
+
+#define EPS 1e-16
+
+#include <cstdio>
+#include <cmath>
+
+int t2_solve (const char *file_a, const char *file_b);
+
+#endif // SOLVE_H
